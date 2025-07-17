23a9702e90317ff86764675e7029b794440c3bec
Michael Urzua <bryanurzua@MICHAEL-URZUA-MB.local>
2025-07-17 09:28:20 -0400

    primer commit

---
+++ api/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
+class User(models.Model):
+    username = models.CharField(max_length=150, unique=True)
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=30, blank=True)
+    last_name = models.CharField(max_length=30, blank=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.username
---
+++ api/views.py
@@ -0,0 +1,11 @@
+from rest_framework import viewsets
+from .models import Task, User
+from .serializers import TaskSerializer, UserSerializer
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
